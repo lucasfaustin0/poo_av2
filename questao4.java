cedcce5a555e5ea6cd7bced260c23bb35fe645c0
lucasfaustin0 <limalucasfaustino@gmail.com>
2025-10-07 20:59:39 -0300

    questao4.java

--- questao4.java
+++ questao4.java
@@ -1,25 +1,53 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o valor a ser pago: ");
+        double valor = sc.nextDouble();
+
+        System.out.print("Escolha o tipo de pagamento: (1 - Cartão | 2 - Boleto): ");
+        int tipo = sc.nextInt();
+
+        Pagamento pagamento;
+        if (tipo == 1) {
+            pagamento = new CartaoCredito(valor);
+        } else {
+            pagamento = new Boleto(valor);
+        }
+
+        pagamento.processarPagamento();
+        sc.close();
+    }
+}
 abstract class Pagamento {
     protected double valor;
+
     public Pagamento(double valor) {
         this.valor = valor;
     }
+
     public abstract void processarPagamento();
+}
+
+class Boleto extends Pagamento {
+
+    Boleto(double valor) {
+        super(valor);
+    }
+    @Override
+    public void processarPagamento() {
+        System.out.println("Pagamento de R$" + valor + " realizado com boleto bancário.");
+    }
 }
 
 class CartaoCredito extends Pagamento {
     public CartaoCredito(double valor) {
         super(valor);
     }
-    public void processarPagamento() {
-        System.out.println("Pagamento de " + valor + "R$ realizado com cartão de crédito.");
-    }
-}
 
-class Boleto extends Pagamento {
-    public Boleto(double valor) {
-        super(valor);
-    }
+    @Override
     public void processarPagamento() {
-        System.out.println("Pagamento de R$" + valor + " realizado via boleto bancário.");
+        System.out.println("Pagamento de R$" + valor + " realizado com cartão de crédito.");
     }
 }
